13cc775441c098a422d0db6299a24bd7db1b527d
Bahareh Kavousi <kavousi.b99@gmail.com>
2023-10-19 19:11:12 +0330

    Email Completed

--- Homeworks/1/CompilerHW1/Program.cs
+++ Homeworks/1/CompilerHW1/Program.cs
@@ -7,11 +7,22 @@ namespace Compiler
     {
         static bool IsEmailValid (string EmailAddress)
         {
-            string EmailPattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+            string EmailPattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+            Regex regex = new Regex(EmailPattern);
+            Match match = regex.Match(EmailAddress);
+            return match.Success;
         }
         static void Main ()
         {
-
+            string email = Console.ReadLine();
+            if (IsEmailValid(email))
+            {
+                Console.WriteLine("Email is valid.");
+            }
+            else
+            {
+                Console.WriteLine("Email is not valid");
+            }
         }
     }
 }
