d47a659c0cded4534a8a9c1dda1e781ba8b5eca6
Bahareh Kavousi nejad <kavousi.b99@gmail.com>
2023-10-19 18:38:36 +0330

    Created HW 1

---
+++ Homeworks/1/CompilerHW1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Compiler
+{
+    class HW1 
+    {
+        static bool IsEmailValid (string EmailAddress)
+        {
+            string EmailPattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+        }
+        static void Main ()
+        {
+
+        }
+    }
+}
